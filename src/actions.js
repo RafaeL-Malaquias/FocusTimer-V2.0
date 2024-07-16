96f7d4287363b799c8d43a932d6e78789da18a4a
RafaeL Malaquias <91689939+RafaeL-Malaquias@users.noreply.github.com>
2024-07-16 22:29:36 +0100

    .

---
+++ src/actions.js
@@ -0,0 +1,12 @@
+export function toggleRunning(){
+    console.log('TogllgeRunning funciton')
+}
+
+
+export function reset(){
+    console.log('função set')
+}
+
+export function turnUp(){
+    console.log('turn up music')
+}
