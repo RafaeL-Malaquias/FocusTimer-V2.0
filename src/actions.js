5796d62b1cc5d8d94b05b8d2be4bdd72cdb17664
RafaeL Malaquias <91689939+RafaeL-Malaquias@users.noreply.github.com>
2024-07-16 22:37:33 +0100

    .

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,14 @@
+import state from './state.js';
+
+
 export function toggleRunning(){
-    console.log('TogllgeRunning funciton')
+     state.isRunning = document.documentElement.classList.toggle('running')
 }
 
 
 export function reset(){
-    console.log('função set')
+    state.isRunning = false
+    document.documentElement.classList.remove('running')
 }
 
 export function turnUp(){
--- src/state.js
+++ src/state.js
@@ -1,5 +1,5 @@
 export default {
-    minutes: 30,
+    minutes: 25,
     seconds: 0,
     isRunning: false,
     isMute: true,
